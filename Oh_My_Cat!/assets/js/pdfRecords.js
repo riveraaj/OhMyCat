f2f1f2fe06fce0276512dd3654d871fc0abb4e18
Jonathan Rivera <96964349+riveraaj@users.noreply.github.com>
2022-08-25 19:40:34 -0600

    Delete Oh_My_Cat! directory

--- Oh_My_Cat!/assets/js/pdfRecords.js
+++
@@ -1,5 +0,0 @@
-const btnDescargar = document.getElementById('descargarExpediente');
-
-btnDescargar.addEventListener('click', function(){
-    window.open('assets/php/pdfRecords.php');
-});
--- Oh_My_Cat!/assets/shared/footerAdministrativo.php
+++
@@ -1,5 +0,0 @@
-<footer>
-    <div class="footer-span">
-        <span id="span-copy">Copyright © 2022 Oh My Cat!</span>
-    </div>
-</footer>
